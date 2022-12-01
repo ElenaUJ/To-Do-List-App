7360dc021966826baea559cdd1f0390600f4b1e9
ElenaUJ <elena-ha@web.de>
2022-12-01 14:05:03 -0500

    Added delete function

--- js/scripts.js
+++ js/scripts.js
@@ -17,4 +17,16 @@ function newItem () {
         li.addClass('strike');
     }
     li.on('dblclick', crossOut);
+
+    // 3(i). Adding the delete button "X"
+    let crossOutButton = $('<crossOutButton>X</crossOutButton>');
+    li.append(crossOutButton);
+    
+    // Question: Why does it not work when I put the event listener below the deleteListItem() function? I though it didn't matter when functions are declared? But when I changed the order it didn't work any more.
+    crossOutButton.on('click', deleteListItem);
+
+    // 3(ii). Adding class="delete" upon click on delete button
+    function deleteListItem () {
+        li.addClass('delete');
+    }
 };
