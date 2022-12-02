57113629fbe63f873ce6c6ac88581854ff0f498e
ElenaUJ <elena-ha@web.de>
2022-12-02 13:53:22 -0500

    Tried to implement fn to add items using Enter key

--- js/scripts.js
+++ js/scripts.js
@@ -36,5 +36,11 @@ function newItem () {
     // 4. Reordering the items
     // Question: So this is only possible with jQuery anyway? I just copied that from the "vanilla" JavaScript template. But I don't have any idea how it works. It's probably very complicated using pure JavaScript?!
     list.sortable();
+}
 
-};
+// Extra: Adding new item upon Enter click
+$(document).on('keydown', function (event) {
+    if ( event.key === Enter ) {
+        newItem();
+    }
+});
