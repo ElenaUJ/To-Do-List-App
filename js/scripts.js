52cd1447955a6ac0785a05829bf2ce7138a2feca
ElenaUJ <elena-ha@web.de>
2022-12-12 10:15:32 -0500

    Deleted clarified comments

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,6 @@ function newItem () {
     let inputValue = userInput.val();
     li.append(inputValue);
 
-    // Question: the vanilla JS version uses if ( inputValue === '' ), is this exactly the same as !inputValue (which is less characters to type that's why I used it)
     if (!inputValue) {
         alert('You must write something!');
     } else {
@@ -25,7 +24,6 @@ function newItem () {
     let crossOutButton = $('<crossOutButton>X</crossOutButton>');
     li.append(crossOutButton);
     
-    // Question: Why does it not work when I put the event listener below the deleteListItem() function? I though it didn't matter when functions are declared? But when I changed the order it didn't work any more.
     crossOutButton.on('click', deleteListItem);
 
     // 3(ii). Adding class="delete" upon click on delete button
@@ -34,7 +32,6 @@ function newItem () {
     }
 
     // 4. Reordering the items
-    // Question: So this is only possible with jQuery anyway? I just copied that from the "vanilla" JavaScript template. But I don't have any idea how it works. It's probably very complicated using pure JavaScript?!
     list.sortable();
 }
 
